5f2297242cabde560c24b17aa477671214412eef
Nikolay Maiorov <maiorovna88@yandex.ru>
2025-05-18 22:06:06 +0300

    Открытие и закрытие формы настроек по клику по кнопке и по клику в не области формы настроек

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,44 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useState } from 'react';
+import stylesArrowButton from 'src/ui/arrow-button/ArrowButton.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	useEffect(() => {
+		function handleClickOutside(evt: MouseEvent) {
+			const target = evt.target as HTMLElement;
+			if (
+				!target.closest(`.${styles.container_open}`) &&
+				!target.closest(`.${stylesArrowButton.container_open}`)
+			) {
+				setIsOpen(false);
+			}
+		}
+		window.addEventListener('click', handleClickOutside);
+		return () => {
+			window.removeEventListener('click', handleClickOutside);
+		};
+	}, []);
+
+	function toggleIsOpen() {
+		setIsOpen((prev) => !prev);
+	}
+	const handlerArrowClick = function () {
+		toggleIsOpen();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					handlerArrowClick();
+				}}
+			/>
+			<aside
+				className={`${styles.container} ${isOpen && styles.container_open}`}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
